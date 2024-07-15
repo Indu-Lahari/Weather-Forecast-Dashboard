6eab81ff0b6982a16fb7a415025fd9a3956f3189
Indu <indulahari6@gmail.com>
2024-07-16 00:32:05 +0530

    Add temperatures and Remove error if place is an empty string

--- backend.py
+++ backend.py
@@ -3,19 +3,15 @@ import requests
 API_KEY = "50752b6f213cf13caf46f5ba51f8f224"
 
 
-def get_data(place, forecast_days, kind):
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     observations = 8 * forecast_days
     filtered_data = filtered_data[:observations]
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    if kind == "Sky":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
     return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Hyderabad", forecast_days=3, kind="Temperature"))
+    print(get_data("place", "forecast_days"))

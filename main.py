9a7c89a9b06c078903f910f058ab2bfdb817b903
Indu <indulahari6@gmail.com>
2024-07-15 16:02:48 +0530

    Data Visualization by creating plot chart

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import streamlit
+import plotly.express as px
 
 streamlit.title("Weather Forecast for the Next Days")
 place = streamlit.text_input("Place: ")
-days = streamlit.slider("Forecast Days", min_value=1, max_value=5,
+days = streamlit.slider("Forecast Days:", min_value=1, max_value=5,
                         help="Select the number of forecasted days")
-option = streamlit.selectbox("Select data to view",
+option = streamlit.selectbox("Select data to view:",
                              ("Temperature", "Sky"))
 streamlit.subheader(f"{option} for the next {days} days in {place}")
+
+
+# Data Visualization
+dates = ["28-10-2023", "29-10-2023", "30-10-2023"]
+temperatures = [37, 36, 43]
+
+
+figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+streamlit.plotly_chart(figure)

6cf5b01f8e510456f1d50cb84fb208c91cf6d87e
Indu <indulahari6@gmail.com>
2024-07-16 00:27:59 +0530

    Add temperatures and Remove error if place is an empty string

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit
 import plotly.express as px
 from backend import get_data
 
+# Add title, text input, slider, select box, and sub header
 streamlit.title("Weather Forecast for the Next Days")
 place = streamlit.text_input("Place: ")
 days = streamlit.slider("Forecast Days:", min_value=1, max_value=5,
@@ -10,9 +11,17 @@ option = streamlit.selectbox("Select data to view:",
                              ("Temperature", "Sky"))
 streamlit.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    # Get the Temperature/Sky data
+    filtered_data = get_data(place, days)
 
+    if option == "Temperature":
+        temperatures = [dict["main"]["temp"] for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        # Create Temperature Plot
+        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+        streamlit.plotly_chart(figure)
 
-d, t = get_data(place, days, option)
-
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-streamlit.plotly_chart(figure)
+    if option == "Sky":
+        sky = [dict["weather"][0]["main"] for dict in filtered_data]
+        streamlit.image()

51c2af59409fc02206d73443851c7467c25d5893
Indu <indulahari6@gmail.com>
2024-07-15 16:04:57 +0530

    Plotting data dynamically using function

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ option = streamlit.selectbox("Select data to view:",
 streamlit.subheader(f"{option} for the next {days} days in {place}")
 
 
-# Data Visualization
-dates = ["28-10-2023", "29-10-2023", "30-10-2023"]
-temperatures = [37, 36, 43]
+def get_data(days):
+    # Data Visualization
+    dates = ["28-10-2023", "29-10-2023", "30-10-2023"]
+    temperatures = [37, 36, 43]
+    temperatures = [days * i for i in temperatures]
+    return dates, temperatures
 
 
-figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+d, t = get_data(days)
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
 streamlit.plotly_chart(figure)
